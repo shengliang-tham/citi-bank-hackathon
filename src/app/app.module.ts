93dbbda35b40f3b7987cf03b1139dafdbcedf1dc
shengliang <ahliang51@hotmail.com>
2021-08-05 01:50:29 +0800

    WIP for cashier scan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { QRScanner } from '@ionic-native/qr-scanner/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -14,6 +15,7 @@ import { AppComponent } from './app.component';
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     BarcodeScanner,
+    QRScanner,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   ],
   bootstrap: [AppComponent],
--- src/app/cashier/tab2/tab2.page.ts
+++ src/app/cashier/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
 
 @Component({
   selector: 'app-tab2',
@@ -10,7 +11,10 @@ export class Tab2Page {
   encodedQR = '';
   scannedData;
 
-  constructor(private barcodeScanner: BarcodeScanner) {}
+  constructor(
+    private barcodeScanner: BarcodeScanner,
+    private qrScanner: QRScanner
+  ) {}
 
   generateQR() {
     this.barcodeScanner.encode('TEXT_TYPE', 'testestest').then((data) => {
@@ -20,14 +24,37 @@ export class Tab2Page {
   }
 
   scanBar() {
-    this.barcodeScanner
-      .scan()
-      .then((barcodeData) => {
-        alert('Barcode data ' + JSON.stringify(barcodeData));
-        this.scannedData = barcodeData;
+    // this.barcodeScanner
+    //   .scan()
+    //   .then((barcodeData) => {
+    //     alert('Barcode data ' + JSON.stringify(barcodeData));
+    //     this.scannedData = barcodeData;
+    //   })
+    //   .catch((err) => {
+    //     console.log('Error', err);
+    //   });
+
+    this.qrScanner
+      .prepare()
+      .then((status: QRScannerStatus) => {
+        if (status.authorized) {
+          // camera permission was granted
+
+          // start scanning
+          const scanSub = this.qrScanner.scan().subscribe((text: string) => {
+            console.log('Scanned something', text);
+
+            this.qrScanner.hide(); // hide camera preview
+            scanSub.unsubscribe(); // stop scanning
+          });
+        } else if (status.denied) {
+          // camera permission was permanently denied
+          // you must use QRScanner.openSettings() method to guide the user to the settings page
+          // then they can grant the permission from there
+        } else {
+          // permission was denied, but not permanently. You can ask for permission again at a later time.
+        }
       })
-      .catch((err) => {
-        console.log('Error', err);
-      });
+      .catch((e: any) => console.log('Error is', e));
   }
 }
