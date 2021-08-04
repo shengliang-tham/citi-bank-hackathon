dbb557a22aa40f39e91f6b8d128f0899cf7a04d4
kaiz-er <kzercube@gmail.com>
2021-08-04 18:33:57 +0800

    citi header

--- src/app/customer/tab2/tab2.page.ts
+++ src/app/customer/tab2/tab2.page.ts
@@ -7,27 +7,5 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
   styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
-  encodedQR = '';
-  scannedData;
-
-  constructor(private barcodeScanner: BarcodeScanner) {}
-
-  generateQR() {
-    this.barcodeScanner.encode('TEXT_TYPE', 'testestest').then((data) => {
-      console.log(data);
-      this.encodedQR = data;
-    });
-  }
-
-  scanBar() {
-    this.barcodeScanner
-      .scan()
-      .then((barcodeData) => {
-        alert('Barcode data ' + JSON.stringify(barcodeData));
-        this.scannedData = barcodeData;
-      })
-      .catch((err) => {
-        console.log('Error', err);
-      });
-  }
+  constructor() {}
 }
