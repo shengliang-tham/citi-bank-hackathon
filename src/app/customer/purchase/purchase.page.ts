87e77e5057c44ef6dc6b32a2c7b46493e845ef2c
seowteng00 <ng_seow_teng@u.nus.edu>
2021-08-05 13:15:42 +0800

    fsdf

--- src/app/customer/purchase/purchase.page.ts
+++ src/app/customer/purchase/purchase.page.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ModalController, ViewWillEnter } from '@ionic/angular';
+import { AllServicesService } from 'src/app/services/all-services.service';
+
+interface Voucher {
+  title: string;
+  points: number;
+  imgUrl: string;
+  qrCode: string;
+  expiryDate: string;
+}
 
 @Component({
   selector: 'app-purchase',
   templateUrl: './purchase.page.html',
   styleUrls: ['./purchase.page.scss'],
 })
-export class PurchasePage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class PurchasePage {
+  constructor(
+    public modalController: ModalController,
+    public _service: AllServicesService
+  ) {}
 }
